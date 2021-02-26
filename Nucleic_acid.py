ba9f939a922fa275306e2000b18c96ecf8b891d8
RSankarMahesh <shama.maha@outlook.com>
2021-02-26 16:06:15 +0530

    'Testfile'

---
+++ Nucleic_acid.py
@@ -0,0 +1,13 @@
+class NA:
+    def __init__(self,seq):
+        self.seq = seq
+    def seq_len(self):
+        print(len(self.seq)) 
+    def category(self):
+        if ('U' in self.seq or 'u' in self.seq) and ('T' in self.seq or 't' in self.seq):
+            print('The sequence cannot contain both U and T')
+        elif ('U' in self.seq or 'u' in self.seq):
+            print('RNA')
+        elif ('T' in self.seq or 't' in self.seq) :
+            print('DNA')    
+    
---
+++ Test_nucleic_acid.py
@@ -0,0 +1,4 @@
+import Nucleic_acid 
+seq = Nucleic_acid.NA('ATGCAUTCGTACGTGTACGATCG')
+seq.seq_len()
+seq.category()
