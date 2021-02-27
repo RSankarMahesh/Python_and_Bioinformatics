eeb6700282aee0e3d4ea0a249033b54cf5860a4f
RSankarMahesh <shama.maha@outlook.com>
2021-02-27 17:39:40 +0530

    Function to count the number of each nucleotide is added

--- Nucleic_acid.py
+++ Nucleic_acid.py
@@ -9,6 +9,14 @@ class NA:
         elif ('U' in self.seq or 'u' in self.seq):
             print('RNA')
         elif ('T' in self.seq or 't' in self.seq) :
-            print('DNA')    
+            print('DNA') 
     def Ncodons(self):
         print('Number of codons will be {}'.format(len(self.seq)/3))
+    def Individual_count(self):
+        UniqueN= str(set(self.seq))
+        for i in range(len(self.seq)):
+            if UniqueN[i] in self.seq:
+                print(UniqueN[i], self.seq.count(UniqueN[i]))
+        
+            
+    
